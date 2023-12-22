263d03bc906ba3c5d11fc55163ccb49ac6080d62
thali-praticando <153683843+thali-praticando@users.noreply.github.com>
2023-12-22 21:21:28 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -14,13 +14,14 @@ selected_chart = st.radio('Selecione o tipo de gráfico:', ('Histograma', 'Gráf
 # Verificar a escolha do usuário e criar o gráfico correspondente
 if selected_chart == 'Histograma':
     # Histograma mais elaborado com cores
-    hist = px.histogram(car_data, x='odometer', color='model_year', title='Histograma de Odômetro com Cores',
+    hist = px.histogram(car_data, x='odometer', color='fuel', title='Histograma de Odômetro',
                         labels={'odometer': 'Odômetro'})
     # Exibir o histograma Plotly interativo
     st.plotly_chart(hist, use_container_width=True)
 else:
     # Gráfico de dispersão com legenda e cores
-    scatter = px.scatter(car_data, x='mileage', y='price', color='model_year', title='Gráfico de Dispersão: Preço x Quilometragem',
-                         labels={'mileage': 'Quilometragem', 'price': 'Preço'})
+    scatter = px.scatter(car_data, x='odometer', y='price', color='fuel', title='Gráfico de Dispersão: Preço x Quilometragem',
+                         labels={'odometer': 'Quilometragem', 'price': 'Preço'})
     # Exibir o gráfico de dispersão Plotly interativo
     st.plotly_chart(scatter, use_container_width=True)
+
